86dec2dc8fe5b3f35b94e7a6bd01685012159ec2
Łukasz Seminowicz <lukasz.seminowicz@gmail.com>
2022-03-29 20:44:40 +0200

    import methods from form-validators.js

--- src/homeworks/hw-day-1/src/validators/form-validators.spec.js
+++ src/homeworks/hw-day-1/src/validators/form-validators.spec.js
@@ -1,3 +1,5 @@
+import { validateRequired, validateMinLength, validateEmail, validatePassword } from './form-validators.js';
+
 describe('form-validators test', () => {
     it('should be...', () => {
         expect(1).toBe(1);
