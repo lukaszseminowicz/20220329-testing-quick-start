9a6d5c25f540b8f7c11b9086562403a13c999192
Łukasz Seminowicz <lukasz.seminowicz@gmail.com>
2022-03-30 08:15:22 +0200

    test for validateRequired

--- src/homeworks/hw-day-1/src/validators/form-validators.spec.js
+++ src/homeworks/hw-day-1/src/validators/form-validators.spec.js
@@ -1,6 +1,11 @@
 import { validateRequired, validateMinLength, validateEmail, validatePassword } from './form-validators.js';
 
 describe('form-validators test', () => {
+    it('should be required', () => {
+        const required = validateRequired('test_field');
+        expect(required.required).toBe('');
+
+    });
     it('should be a valid email address', () => {
         const email = validateEmail('test.email@gmail.com');
         expect(email.email).toBe('test.email@gmail.com');
