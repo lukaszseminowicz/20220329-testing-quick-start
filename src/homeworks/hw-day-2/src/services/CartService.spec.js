17e8bba37a7b359e757ede53df82a16dc72297ce
Łukasz Seminowicz <lukasz.seminowicz@gmail.com>
2022-03-31 08:04:23 +0200

    remove comments

--- src/homeworks/hw-day-2/src/services/CartService.spec.js
+++ src/homeworks/hw-day-2/src/services/CartService.spec.js
@@ -3,12 +3,10 @@ import {CartService} from './CartService';
 describe('Cart Service', () => {
     let cartService;
     beforeEach(() => {
-        // TODO
         cartService = new CartService();
     });
 
     it('should should be ...', () => {
-        // const cartService = new CartService();
         const addFirstItem = cartService.addToCart({id: 2, name: 'Raspberries', value: 20.22, imgUrl: 'https://picsum.photos/id/102/800/600'});
         // const addSecondItem = cartService.addToCart({id: 4, name: 'Smartphone', value: 452.99, imgUrl: 'https://picsum.photos/id/3/800/600'});
         const allItems = cartService.getAll();
